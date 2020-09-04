c7f1162fc189e4abf447fc380c615fe9f71cc67e
Dan Quackenbush <dan.quackenbush@contrastsecurity.com>
2020-09-04 09:49:30 -0400

    Create additional steps for senior candidates

---
+++ project2/flask/vuln.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_data():
+    """
+    TODO: Implement code that will return vulnerabilities, as described in the README
+    """
+    pass
