d0bd97064fbe6cd75da7663c4d257b7167c01564
asquare4120 <aditya.kushwaha89@gmasil.com>
2022-02-16 09:49:46 -0800

    add s3 modified without backend config

--- project2/terraform/s3.tf
+++ project2/terraform/s3.tf
@@ -1,28 +1,11 @@
 
 resource "aws_s3_bucket" "vancouver-canada" {
-   bucket = var.s3bucket
+   bucket = var.bucket
    force_destroy = true
    #region = var.region
-   versioning {
-        enabled = true
-    }
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm = "AES256"
-            }
-        }
-    }
-    object_lock_configuration {
-        object_lock_enabled = "Enabled"
-    }
-    tags = {
-        Name = "S3 Remote Terraform State Store"
-    }
 }
 
-
-
+/*
 resource "aws_dynamodb_table" "terraform-lock" {
     name           = "terraform_state"
     read_capacity  = 5
@@ -37,11 +20,29 @@ resource "aws_dynamodb_table" "terraform-lock" {
     }
 }
 
+
 terraform {
   backend "s3" {
-    bucket         = var.s3bucket
     key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform_state"
+    bucket         = "terraform-up-and-running-statezpl"
+    region         = "us-west-1"
+    #dynamodb_table = "terraform-up-and-running-locks"
   }
+}
+
+/*
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+*/
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
 }
